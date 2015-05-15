2a23ee4edff9b8d75d41f7c0d1fe08b635dd5a19
Dan Thareja <danthareja@gmail.com>
2015-05-14 22:09:42 -0700

    init

---
+++ src/scope.js
@@ -0,0 +1,20 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function Scope() {
+  this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+  _.forEach(this.$$watchers, function(watcher) {
+    watcher.listenerFn();
+  });
+};
