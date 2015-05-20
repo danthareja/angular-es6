a799831ce1528b1cc00fd8d38001cf21ad77a8af
Dan Thareja <danthareja@gmail.com>
2015-05-19 22:48:58 -0700

    unstable digest and optimizations

--- src/scope.js
+++ src/scope.js
@@ -1,23 +1,48 @@
+import _ from 'lodash';
+
 class Scope {
   constructor() {
     this.$$watchers = [];
+    this.$$lastDirtyWatch = null;
   }
 
   $watch(watchFn, listenerFn) {
-    var watcher = { watchFn, listenerFn, last: initWatchValue };
+    var watcher = { 
+      watchFn, // Object literal shorthand can be partially applied
+      listenerFn: listenerFn || () => {},
+      last: initWatchValue
+    };
     this.$$watchers.push(watcher);
+    this.$$lastDirtyWatch = null;
   }
 
-  $digest() {
-    var newValue, oldValue;
-    this.$$watchers.forEach(watcher => {
+  $$digestOnce() {
+    var newValue, oldValue, dirty;
+    _.each(this.$$watchers, watcher => {
       newValue = watcher.watchFn(this); // Arrow function has lexical 'this'
       oldValue = watcher.last;
       if (newValue !== oldValue) {
+        this.$$lastDirtyWatch = watcher;
         watcher.last = newValue;
         watcher.listenerFn(newValue, oldValue === initWatchValue ? newValue : oldValue, this);
+        dirty = true;
+      } else if (this.$$lastDirtyWatch === watcher) {
+        return false; // explicit return breaks out of lodash's _.each
       }
     });
+    return dirty;
+  }
+
+  $digest() {
+    var ttl = 10;
+    var dirty;
+    this.$$lastDirtyWatch = null;
+    do {
+      dirty = this.$$digestOnce();
+      if (dirty && !(ttl--)) {
+        throw `${ttl} digest cycles reached`;
+      }
+    } while(dirty);
   }
 }
 
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -2,24 +2,24 @@ import _ from 'lodash';
 import $ from 'jquery';
 import Scope from '../src/scope.js';
 
-describe("Scope", () => {
+describe('Scope', () => {
 
-  it("can by constructed and used as an object", () => {
+  it('can by constructed and used as an object', () => {
     let scope = new Scope();
     scope.aProperty = 1;
 
     expect(scope.aProperty).toBe(1);
   });
 
-  describe("digest", () => {
+  describe('digest', () => {
     let scope;
 
     beforeEach(() => {
       scope = new Scope();
     });
 
-    it("calls the listener function on first $digest", () => {
-      let watchFn = () => "wat";
+    it('calls the listener function on first $digest', () => {
+      let watchFn = () => 'wat';
       let listenerFn = jasmine.createSpy();
       scope.$watch(watchFn, listenerFn);
 
@@ -31,7 +31,7 @@ describe("Scope", () => {
 
     it('calls the watch function with the scope as the arugment', () => {
       let watchFn = jasmine.createSpy();
-      let listenerFn = () => {}; // noop
+      let listenerFn = () => {/* noop */};
       scope.$watch(watchFn, listenerFn);
 
       scope.$digest();
@@ -39,7 +39,7 @@ describe("Scope", () => {
       expect(watchFn).toHaveBeenCalledWith(scope);
     });
 
-    it("calls the listener function when the watched value changes", () => {
+    it('calls the listener function when the watched value changes', () => {
       scope.someValue = 'a';
       scope.counter = 0;
 
@@ -87,5 +87,99 @@ describe("Scope", () => {
       scope.$digest();
       expect(oldValueGiven).toBe(123);
     });
+
+    it('may have watchers that omit the listener function', () => {
+      let watchFn = jasmine.createSpy().and.returnValue('something');
+      scope.$watch(watchFn);
+
+      scope.$digest();
+
+      expect(watchFn).toHaveBeenCalled();
+    });
+
+    it('triggers chained watchers in the same digest', () => {
+      scope.name = 'Dan';
+
+      scope.$watch(
+        (scope) => scope.nameUpper,
+        (newValue, oldValue, scope) => {
+          if (newValue)
+            scope.initial = newValue.substring(0,1) + '.';
+        }
+      );
+
+      scope.$watch(
+        (scope) => scope.name,
+        (newValue, oldValue, scope) => {
+          if (newValue)
+            scope.nameUpper = newValue.toUpperCase();
+        }
+      );
+
+      scope.$digest();
+      expect(scope.initial).toBe('D.');
+
+      scope.name = 'Bob';
+      scope.$digest();
+      expect(scope.initial).toBe('B.');
+    });
+
+    it('gives up on the watches after 10 iterations', () => {
+      scope.counterA = 0;
+      scope.counterB = 0;
+
+      scope.$watch(
+        (scope) => scope.counterA,
+        (newValue, oldValue, scope) => scope.counterB++
+      );
+
+      scope.$watch(
+        (scope) => scope.counterB,
+        (newValue, oldValue, scope) => scope.counterA++
+      );
+
+      expect(() => scope.$digest()).toThrow();
+    });
+
+    it('ends the digest when the last watch is clean', () => {
+      scope.array = _.range(100);
+      let watchExecutions = 0;
+
+      _.times(100, i => {
+        scope.$watch(
+          (scope) => {
+            watchExecutions++;
+            return scope.array[i];
+          },
+          (newValue, oldValue, scope) => {}
+        );
+      });
+
+      scope.$digest();
+      expect(watchExecutions).toBe(200);
+
+      scope.array[0] = 420;
+      scope.$digest();
+      expect(watchExecutions).toBe(301);
+    });
+
+    it('does not end digest so that new watches are not run', () => {
+      scope.aValue = 'abc';
+      scope.counter = 0;
+
+      scope.$watch(
+        (scope) => scope.aValue,
+        (newValue, oldValue, scope) => {
+          scope.$watch(
+            (scope) => scope.aValue,
+            (newValue, oldValue, scope) => scope.counter++
+          );
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+    });
+
   });
 });
