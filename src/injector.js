7117e8c1dddad8aa594e6c6d0e12383f255309b8
Dan Thareja <danthareja@gmail.com>
2015-05-26 07:39:56 -0700

    inject multiple modules

--- src/injector.js
+++ src/injector.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 function createInjector(modulesToLoad) {
   let cache = new Map();
+  let loadedModules = new Set();
 
   let $provide = {
     // Shorthand object literal method creation, same as
@@ -14,11 +15,16 @@ function createInjector(modulesToLoad) {
     }
   };
 
-  _.each(modulesToLoad, (moduleName)=> {
-    let module = angular.module(moduleName);
-    // Array destructuring: _invokeQueue[0] = method, _invokeQueue[1] = args
-    // Spread ...args into method $provide[method] instead of apply
-    _.each(module._invokeQueue, ([method, args])=> $provide[method](...args));
+  _.each(modulesToLoad, function loadModule(moduleName) {
+    if (!loadedModules.has(moduleName)) {
+      loadedModules.add(moduleName);
+      let module = angular.module(moduleName);
+      
+      _.each(module.requires, loadModule);
+      // Array destructuring: _invokeQueue[0] = method, _invokeQueue[1] = args
+      // Spread ...args into method $provide[method] instead of apply
+      _.each(module._invokeQueue, ([method, args])=> $provide[method](...args));
+    }
   });
 
   return {
